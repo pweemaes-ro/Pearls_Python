0da00964084c919351cf706869cf9978d7eacf4d
pweemaes-ro <zappamotel@quicknet.nl>
2023-08-04 20:45:50 +0200

    Just for fun... A simple dice sim.

--- dice_anina.py
+++ dice_anina.py
@@ -3,21 +3,20 @@
 
 class Dice(dict[str, str]):
 	"""Simple class to mimic a dice. It can roll (in one of four directions at
-	a time), its faces can be queried for their values, and it can produce a
+	a time), its faces can be queried for their value, and it can produce a
 	string representation of itself."""
 	
-	__faces = 'UDLRFB'
-	__transforms = {'L': "ULDRUFFBB", 'R': "RDLURFFBB", 'F': "UFDBULLRR",
-	                'B': "BDFUBLLRR"}
+	__transforms = {'L': "ULDRUFFBB", 'R': "RDLURFFBB",
+	                'F': "UFDBULLRR", 'B': "BDFUBLLRR"}
 
 	def __init__(self, faces: tuple[str, ...], values: tuple[str, ...]) -> None:
 		super().__init__(zip(values, faces))
 		self._faces_and_values = dict(zip(faces, values))
 		
-	def roll(self, roll: str) -> None:
+	def roll(self, direction: str) -> None:
 		"""Roll in specified direction ('L', 'R', 'F', or 'D')."""
 		
-		transform = self.__transforms[roll]
+		transform = Dice.__transforms[direction]
 		new_dice = dict(self)
 		for value, current_face in self.items():
 			new_dice[value] = transform[(transform.find(current_face) + 1)]
@@ -25,8 +24,7 @@ class Dice(dict[str, str]):
 		self.update(new_dice)
 	
 	def face_to_value(self, face: str) -> str:
-		"""Return the current value at face. Face is expected to be in
-		('U', 'D', 'L', 'R', 'F', 'B')."""
+		"""Return value at face (must be in ('U', 'D', 'L', 'R', 'F', 'B'))."""
 
 		return self._faces_and_values[face]
 
@@ -38,8 +36,8 @@ if __name__ == "__main__":
 
 	dice = Dice(_faces := tuple('BRFULD'), _values := tuple('ABCDEF'))
 
-	for _roll in 'LLFFRR':
-		dice.roll(_roll)
+	for roll_direction in 'LLFFRR':
+		dice.roll(roll_direction)
 
 	for _face in _faces:
 		print(f"Value on {_face} face = {dice.face_to_value(_face)}")
