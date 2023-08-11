d300a997e57bb35b290f29b03b5f83483b4435fd
pweemaes-ro <zappamotel@quicknet.nl>
2023-08-11 16:27:50 +0200

    Cosmetic changes

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -4,14 +4,15 @@ from random import choice
 from dice_anina import Dice
 from dice_anina2 import roll
 
-rolls = "LRFB"
+directions = "LRFB"
 roll_inverse = {"L": "R", "R": "L", "F": "B", "B": "F"}
 
 
 def _generate_symmetric_rolls(half_length: int) -> str:
 	"""Return s rolls string that results in 'identity-roll', by first doing a
 	number of rolls and then inverting (undo-ing) them."""
-	half = ''.join(choice(rolls) for _ in range(half_length))
+	
+	half = ''.join(choice(directions) for _ in range(half_length))
 	return half + ''.join(roll_inverse[k] for k in half[::-1])
 
 
@@ -22,14 +23,14 @@ def test_dice_rolls_class() -> None:
 		Dice(_faces := tuple('BRFULD'), _values := tuple('ABCDEF'))
 
 	# do the rolls
-	for roll_sets in {"L" * 4, "R" * 4, "F" * 4, "B" * 4}:
-		for roll_direction in roll_sets:
+	for test_roll in ("L" * 4, "R" * 4, "F" * 4, "B" * 4):
+		for roll_direction in test_roll:
 			dice_class.roll(roll_direction)
 		assert dice_class == original_dice_class
 
 	for i in range(10):
-		roll_sets = _generate_symmetric_rolls(15)
-		for roll_direction in roll_sets:
+		test_roll = _generate_symmetric_rolls(15)
+		for roll_direction in test_roll:
 			dice_class.roll(roll_direction)
 		assert dice_class == original_dice_class
 
@@ -41,13 +42,13 @@ def test_dice_rolls_functional() -> None:
 		[[*"CF"], [*"BR"], [*"AB"], [*"FD"], [*"EL"], [*"DU"]]
 
 	# do the rolls
-	for roll_sets in {"L" * 4, "R" * 4, "F" * 4, "B" * 4}:
-		for roll_direction in roll_sets:
+	for test_roll in ("L" * 4, "R" * 4, "F" * 4, "B" * 4):
+		for roll_direction in test_roll:
 			roll(dice_functional, roll_direction)
 		assert dice_functional == original_dice_functional
 
 	for i in range(10):
-		roll_sets = _generate_symmetric_rolls(15)
-		for roll_direction in roll_sets:
+		test_roll = _generate_symmetric_rolls(15)
+		for roll_direction in test_roll:
 			roll(dice_functional, roll_direction)
 		assert dice_functional == original_dice_functional
